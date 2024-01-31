c0411fbc5649ee462018b0a0d66f69ba54411813
muttaqin-md <takeen9877@gmail.com>
2024-02-01 01:43:43 +0800

    Prediction Pipeline

--- src/Components/data_ingestion.py
+++ src/Components/data_ingestion.py
@@ -13,7 +13,6 @@ from src.Components.data_transformation import DataTransformationConfig
 from src.Components.model_trainer import ModelTrainerConfig
 from src.Components.model_trainer import ModelTrainer
 
-
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
@@ -56,7 +55,7 @@ if __name__=="__main__":
     train_data,test_data=obj.initiate_data_ingestion()
 
     data_transformation=DataTransformation()
-    train_arr,test_arr,_= data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
 
     modeltrainer=ModelTrainer()
     print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
