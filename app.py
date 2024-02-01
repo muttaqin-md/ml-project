bbe4e957648c08095c25c3b95d041d661929995c
muttaqin-md <takeen9877@gmail.com>
2024-02-02 00:18:39 +0800

    Deploment Configuration for AWS

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def predict_datapoint():
     
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0", debug=True)        
+    app.run(host="0.0.0.0")        
 
 
 ##http://127.0.0.1:5000/predictdata 
